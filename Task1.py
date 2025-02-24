e432917659969dce0d10d3034297ee0e32d5cc36
Marion Le Parc <marion.le-parc@ecl21.ec-lyon.fr>
2025-02-25 00:48:54 +0100

    Final Marion's version of Task 1
    I added the different demands with "random" bid prices, modified the constraints and the objective function.
    And I got rid of the unused parameters and variables

--- Task1.py
+++ Task1.py
@@ -1,15 +1,12 @@
 from pyomo.environ import *
 import all_data
 import numpy as np
+import matplotlib.pyplot as plt
 
 #import data
 data=all_data.get_data()
 Pmax_D=np.max(data["load"]["System demand (MW)"])
-#P_max_wind=[np.mean(values) for values in data["wind_farm"].values()]
 wind_farm_capacity=200
-
-U_d=20 #Demand bid price
-
 
 #create a model
 model = ConcreteModel()
@@ -17,28 +14,32 @@ model = ConcreteModel()
 # Sets
 model.init_conv_G = Set(initialize=[e+1 for e in range(12)])
 model.init_wind_farm = Set(initialize=[e+1 for e in range(6)])
+model.init_demand=Set(initialize=[e+1 for e in range(len(data["node_demand"]["Load #"]))])
 
 
 #Declare variables
-model.p_demand= Var(within=NonNegativeReals, bounds=(0, Pmax_D), initialize=Pmax_D/2)
+model.p_demand= Var(model.init_demand, within=NonNegativeReals, initialize=Pmax_D/2)
 model.p_conv_G = Var(model.init_conv_G, within=NonNegativeReals, initialize=0)
 model.p_wind_farm = Var(model.init_wind_farm, within=NonNegativeReals, initialize=0)
 
 #Parameters
 model.Pmax_convG=Param(model.init_conv_G, initialize={i+1: pmax for i, pmax in enumerate(data["generation_unit"]["Pmax (MW)"])})
-model.Pmin_convG=Param(model.init_conv_G, initialize={i+1: pmin for i, pmin in enumerate(data["generation_unit"]["Pmin (MW)"])})
-model.Pini_convG=Param(model.init_conv_G, initialize={i+1: pmax for i, pmax in enumerate(data["generation_unit"]["Pini_i"])})
-
 model.price_conv=Param(model.init_conv_G, initialize={i+1: price for i, price in enumerate(data["generation_unit"]["Ci"])})
+
 model.price_wind_farm=Param(model.init_wind_farm, initialize = 0)
 
+model.Pmax_demand=Param(model.init_demand, initialize={i+1: price for i, price in enumerate(data["node_demand"]["Load distribution peak"])} )
+model.demand_bidding_prices=Param(model.init_demand, initialize={i+1: price for i, price in enumerate(data["node_demand"]["Bid price"])})
 
-#Objective function : Maximization of Social welfare
+
+# Objective function : Maximization of Social welfare
 def objective_rule(model):
-    return (U_d*model.p_demand - sum(model.price_conv[i] * model.p_conv_G[i] for i in model.init_conv_G) - sum(model.price_wind_farm[j] * model.p_wind_farm[j] for j in model.init_wind_farm))
+    return (sum(model.demand_bidding_prices[k]*model.p_demand[k] for k in model.init_demand)
+            - sum(model.price_conv[i] * model.p_conv_G[i] for i in model.init_conv_G) 
+            - sum(model.price_wind_farm[j] * model.p_wind_farm[j] for j in model.init_wind_farm))
 model.social_welfare = Objective(rule=objective_rule, sense=maximize)
 
-#constraints
+# Constraints
 def capacity_rule_conv_G(model, i):
     return model.p_conv_G[i] <= model.Pmax_convG[i]
 model.capacity_conv_G_constraint = Constraint(model.init_conv_G, rule=capacity_rule_conv_G)
@@ -47,11 +48,15 @@ def capacity_rule_wind_farm(model, i):
     return model.p_wind_farm[i] <= np.mean(data["wind_farm"][f"wind_farm {i}"])*wind_farm_capacity
 model.capacity_wind_farm_constraint = Constraint(model.init_wind_farm, rule=capacity_rule_wind_farm)
 
+def max_load_demand(model, j):
+    return model.p_demand[j] <= model.Pmax_demand[j]
+model.max_load_demand = Constraint(model.init_demand, rule=max_load_demand)
+
 def balance(model):
-    return model.p_demand == sum(model.p_conv_G[i] for i in model.init_conv_G) + sum(model.p_wind_farm[j] for j in model.init_wind_farm)
+    return sum(model.p_demand[k] for k in model.init_demand) == sum(model.p_conv_G[i] for i in model.init_conv_G) + sum(model.p_wind_farm[j] for j in model.init_wind_farm)
 model.balance_constraint = Constraint(rule=balance)
 
-# To get the dual variables
+#Dual variables
 model.dual = Suffix(direction=Suffix.IMPORT)
 
 # Create a solver 
@@ -60,16 +65,15 @@ solver = SolverFactory("gurobi", solver_io="python")  # Make sure Gurobi is inst
 # Solve the model
 solution = solver.solve(model, tee=True)
 
-
 model.display()
 
 # Store the results
 results = {}
-results["load"] = [value(model.p_demand[key]) for key in model.p_demand]
-results["conventional generators"] = [value(model.p_conv_G[key]) for key in model.p_conv_G]
-results["wind farms"] = [value(model.p_wind_farm[key]) for key in model.p_wind_farm]
+results["load"] = [round(value(model.p_demand[key]),3) for key in model.p_demand]
+results["conventional generators"] = [round(value(model.p_conv_G[key]),3) for key in model.p_conv_G]
+results["wind farms"] = [round(value(model.p_wind_farm[key]),3) for key in model.p_wind_farm]
 print(results)
-print("Social Welfare =", model.social_welfare())
+print("Social Welfare =", round(model.social_welfare(), 3))
 
 #print(solution)
 
@@ -80,14 +84,53 @@ if solution.solver.termination_condition == TerminationCondition.optimal:
 else:
     print("Solver did not find an optimal solution. MCP cannot be calculated.")
 
-#Profit of each generators and Utility
+#Profit of each generators
 profit= {}
 profit["conventional generators"] = [
-    prod * (mcp - price)
+    round(prod * (mcp - price), 3)
     for prod, price in zip(results["conventional generators"], data["generation_unit"]["Ci"])]
+profit["wind farms"] = [round(value * mcp, 3) for value in results["wind farms"]]
 
-profit["wind farms"] = [value * mcp for value in results["wind farms"]]
-
-utility=[value *(U_d-mcp) for value in results["load"]]
+#Utility
+utility= [round(qtt*(bidprice-mcp), 3) for bidprice, qtt in zip(data["node_demand"]["Bid price"], results["load"])]
 
 print("profit", profit, "utility", utility)
+
+
+####### Print Supply and Offer curves
+
+# Data
+
+demand_quantities=[2650.5*x/100 for x in [3.8, 3.4, 6.3, 2.6, 2.5, 4.8, 4.4, 6.0, 6.1, 6.8, 9.3, 6.8, 11.1, 3.5, 11.7, 6.4, 4.5, 0]]
+demand_bidding_prices=[25, 22, 15.4, 12.5, 13, 14, 24, 15, 12.8, 17.8, 29.3, 28, 16.9, 30, 18, 16, 21, 0]
+
+supply_quantities = [152, 152, 350, 591, 60, 155, 155, 400, 400, 300, 300, 350, 
+                     2*66.02668138964893, 2*69.90705836674121, 2*69.09000109053993, 
+                     2*60.93791955818926, 2*64.88591062635679, 2*62.98944854324062]
+generators_bidding_prices = [13.32, 13.32, 20.7, 20.93, 26.11, 10.52, 6.02, 5.47, 0, 0, 
+                  10.52, 10.89, 0, 0, 0, 0, 0, 0]
+
+# Plot the supply curve
+sorted_indices = np.argsort(generators_bidding_prices) #Sort in ascending order
+sorted_prices = np.array(generators_bidding_prices)[sorted_indices]
+sorted_supply= np.array(supply_quantities)[sorted_indices]
+sorted_cumulative_supply = np.cumsum(sorted_supply) 
+
+plt.figure(figsize=(10, 6))
+plt.step(sorted_cumulative_supply, sorted_prices, where='post', label="Supply", color='orange')
+
+#Plot the demand curve
+sorted_indices_dem = np.argsort(demand_bidding_prices)[::-1]
+sorted_prices_dem = np.array(demand_bidding_prices)[sorted_indices_dem]
+sorted_demand= np.array(demand_quantities)[sorted_indices_dem]
+sorted_cumulative_demand = np.cumsum(sorted_demand) 
+plt.step(sorted_cumulative_demand, sorted_prices_dem, where='post', label="Demand", color='blue')
+
+# Add legends
+plt.ylabel("Price")
+plt.xlabel("Quantity (MW)")
+plt.title("Supply and Demand")
+plt.legend()
+plt.grid(True)
+
+plt.show()
--- all_data.py
+++ all_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def get_data():
     
@@ -27,80 +28,26 @@ def get_data():
     "Uini_i": [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1],
     "Tini_i": [22, 22, -2, -1, -1, -1, 10, -2, -2, -1, 24, 50]}
 
-    #Demand data
+    # Demand data from pdf file
     load_data = {
     "Hour": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
     "System demand (MW)": [
         1775.835, 1669.815, 1590.3, 1563.795, 1563.795, 1590.3, 1961.37, 2279.43, 
         2517.975, 2544.48, 2544.48, 2517.975, 2517.975, 2517.975, 2464.965, 2464.965, 
-        2623.995, 2650.5, 2650.5, 2544.48, 2411.955, 2199.915, 1934.865, 1669.815
-    ]}
+        2623.995, 2650.5, 2650.5, 2544.48, 2411.955, 2199.915, 1934.865, 1669.815 ]}
+
+    # Node Location and Distribution of the Total System Demand
+    node_demand = {
+    "Load #": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    "Node": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 18, 19, 20],
+    "percentage of system load": [3.8, 3.4, 6.3, 2.6, 2.5, 4.8, 4.4, 6.0, 6.1, 6.8, 9.3, 6.8, 11.1, 3.5, 11.7, 6.4, 4.5 ],
+    "Load distribution peak" : [np.max(load_data["System demand (MW)"])*x/100 for x in [3.8, 3.4, 6.3, 2.6, 2.5, 4.8, 4.4, 6.0, 6.1, 6.8, 9.3, 6.8, 11.1, 3.5, 11.7, 6.4, 4.5]],
+    "Bid price" : [25, 22, 15.4, 12.5, 13, 14, 24, 15, 12.8, 17.8, 29.3, 28, 16.9, 30, 18, 16, 21]}
+    
     #Wind farms data : We extract the production of the 24 first hours of each first dataset from each zone (1 zone 1 file)
     wind_farm = {}
     file_path = ["Wind farms data\scen_zone1.out", "Wind farms data\scen_zone2.out","Wind farms data\scen_zone3.out","Wind farms data\scen_zone4.out","Wind farms data\scen_zone5.out","Wind farms data\scen_zone6.out"]
     for i in range(len(file_path)):
         wind_farm[f"wind_farm {i+1}"] = pd.read_csv(file_path[i])["V1"].head(24).tolist()
 
-    return {"generation_unit" : generating_unit_data, "load" : load_data, "wind_farm" : wind_farm}
-
-
-test=get_data()
-
-# print(np.max(test["load"]["System demand (MW)"])) #pour avoir la Demand max
-# print([e+1 for e in range(len(test["generation_unit"]["Unit #"]))])
-# print(len(test["generation_unit"]["Unit #"]))
-
-print(np.mean(test["wind_farm"]["wind_farm 1"])*200)  #200 is the capacity
-# print([np.mean(values) for values in test["wind_farm"].values()])
-
-offre=[152, 152, 350, 591, 60, 155, 155, 400, 400, 300, 300, 350]
-for i in range(1,len(test["wind_farm"])+1):
-       offre.append(100*np.mean(test["wind_farm"][f"wind_farm {i}"]))
-print(offre)
-    
-
-
-####### Print Supply and Offer curves
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Données
-#demand = 2650.5  # MW
-demand=[2650.5,2550.5, 2500, 1900, 1530, 1200, 800, 648, 500, 200, 0]
-demand_bidding_prices=[0,10.39, 12.2, 12.8, 13.4, 14.0, 16.3, 18.2, 20.1, 24, 24]
-
-
-supply_quantities = [152, 152, 350, 591, 60, 155, 155, 400, 400, 300, 300, 350, 
-                     2*66.02668138964893, 2*69.90705836674121, 2*69.09000109053993, 
-                     2*60.93791955818926, 2*64.88591062635679, 2*62.98944854324062]
-generators_bidding_prices = [13.32, 13.32, 20.7, 20.93, 26.11, 10.52, 6.02, 5.47, 0, 0, 
-                  10.52, 10.89, 0, 0, 0, 0, 0, 0]
-
-# Tracer l'offre
-
-sorted_indices = np.argsort(generators_bidding_prices)       # Trier par prix croissant
-print("ind",sorted_indices)
-sorted_prices = np.array(generators_bidding_prices)[sorted_indices]
-print("pri",sorted_prices)
-sorted_supply= np.array(supply_quantities)[sorted_indices]
-sorted_cumulative_supply = np.cumsum(sorted_supply) 
-
-# Plot the supply curve
-plt.figure(figsize=(10, 6))
-plt.step(sorted_cumulative_supply, sorted_prices, where='post', label="Supply", color='orange')
-
-# # Tracer la demande constante
-# plt.axvline(x=demand, color='blue', linestyle='--', label="Demand (2650 MW)")
-
-#Plot the demand curve
-plt.step(demand, demand_bidding_prices, where='post', label="Demand", color='blue')
-
-# Ajouter des annotations et des styles
-plt.ylabel("Price")
-plt.xlabel("Quantity (MW)")
-plt.title("Supply and Demand")
-plt.legend()
-plt.grid(True)
-
-# Afficher le graphique
-plt.show()
+    return {"generation_unit" : generating_unit_data, "load" : load_data,  "wind_farm" : wind_farm, "node_demand" : node_demand}
